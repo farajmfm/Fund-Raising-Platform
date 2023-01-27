967c6f5e6da577c4f6fb533fad3af33ac4c9d737
mohomed faraj <86347430+farajmfm@users.noreply.github.com>
2023-01-27 09:32:49 +0530

    navbar

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { logo, sun } from '../assets';
+import { navlinks } from '../constants';
+
+const Sidebar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sidebar
