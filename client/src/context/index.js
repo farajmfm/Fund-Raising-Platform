013596cf5adde47949e01a1c31f717a9cacf9a48
mohomed faraj <86347430+farajmfm@users.noreply.github.com>
2023-01-29 15:49:16 +0530

    context

---
+++ client/src/context/index.js
@@ -0,0 +1,35 @@
+import React, { useContext, createContext } from 'react';
+
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+    const {contract} = useContract
+    ('0x1DbaC08cb2e3AFf12ebaE78C2E69F23eab03D8a3');
+    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createContract');
+
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+        try {
+            const data = await createCampaign([
+                //We can get it from createCampaign in CrowdFunding.sol 
+                address, //owner
+                form.title, //title
+                form.description, //description
+                form.target, 
+                new Date(form.deadline).getTime(), //deadline
+                form.image
+            ])
+
+            console.log("Contract Call Successfully", data)
+
+        } catch (error) {
+            console.log("Contract Call Unsuccessfully", error)
+        }
+    }
+
+}
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -32,7 +32,7 @@ const CreateCampaign = () => {
     <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] p-10 p-4">
       {isLoading && 'Loader...'}
       <div className="flex justify-center items-center p-[16px] min-w-[380px] bg-[#3a3a43] rounded-[10px]">
-        <h1 className="font-epilogue font-bold text-[25px] text-[18px] leading-[38px] text-white">
+        <h1 className="font-epilogue font-bold text-[25px] leading-[38px] text-white">
           Start a Campaign
         </h1>
       </div>
